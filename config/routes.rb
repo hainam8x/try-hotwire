bae7d9d4e1de65ade0cd84fdc6c25dc8be48cc34
Nam Hoang <hainam8x@gmail.com>
2021-04-22 08:03:31 +0700

    up

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
-  resources :tickets
+  resources :tickets do 
+  	resource :comments, only: [:new, :create]
+  end
+
+  root to: "tickets#index"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
